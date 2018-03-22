e484cf518acc636452a6cd146789ddb893f175be
Deepak Sahu <sahu.deepak01@gmail.com>
2018-03-22 20:05:45 +0530

    SahuCoin solidity file

---
+++ SahuCoin.sol
@@ -0,0 +1,7 @@
+pragma solidity 0.4.19;
+import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
+contract SahuCoin is MintableToken {
+    string public name = "Sahu COIN";
+    string public symbol = "DEEP";
+    uint8 public decimals = 18;
+}
